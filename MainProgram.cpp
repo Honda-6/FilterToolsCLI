5dfd96f70382fef1d53b8b7be08f545102e83d3f
Nada Gamal <147168082+Nada-08@users.noreply.github.com>
2023-10-20 11:18:47 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -1,3 +1,8 @@
+// Last Modification Date:	                        20/10/2023
+// Author1 "Nada Gamal Ismail Khalil":	            20220359
+// Author2 "Farah Ali Mohamed Ali":	                20220497
+// Author3 "Mohannad Al Sayed Attia El Bendary":	20220347
+
 #include"Filters.cpp"
 
 
